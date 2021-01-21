adb6528e446901e67cef5e39fa50ee7aae786332
Archit Bhonsle <abhonsle2000@gmail.com>
2021-01-21 14:04:03 +0530

    🔨 changing shape of response

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -16,25 +16,22 @@ router.post('/login', async (req, res) => {
     const doc = await Admin.findOne({ username });
     if (doc === null) {
       res.send({
-        data    : null,
-        error   : [ { field: 'username', message: "user doesn't exist" } ],
-        message : 'login failed'
+        data  : null,
+        error : [ { field: 'username', message: "user doesn't exist" } ]
       });
       return;
     }
     if (!await argon2.verify(doc.password, password)) {
       res.send({
-        data    : null,
-        error   : [ { field: 'password', message: 'incorrect password' } ],
-        message : 'login failed'
+        data  : null,
+        error : [ { field: 'password', message: 'incorrect password' } ]
       });
     }
     req.session.username = doc.username;
     req.session.save();
     res.send({
-      data    : { user: { username: doc.username, name: doc.name } },
-      error   : null,
-      message : 'login successful'
+      data  : { user: { username: doc.username, name: doc.name } },
+      error : null
     });
   } catch (error) {
     console.error(error);
@@ -45,9 +42,9 @@ router.post('/logout', isAuth, (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       console.log(err);
-      res.send({ data: null, error: err.message, message: 'logout failed' });
+      res.send({ data: null, error: 'unknown error occurred' });
     } else {
-      res.send({ data: null, error: null, message: 'logout successful' });
+      res.send({ data: true, error: null });
     }
   });
 });
@@ -58,16 +55,14 @@ router.get('/', isAuth, async (req, res) => {
     const user = await Admin.findOne({ username });
     if (user === null) {
       res.send({
-        data    : null,
-        error   : "user doesn't exist",
-        message : 'failed'
+        data  : null,
+        error : "user doesn't exist"
       });
       return;
     }
     res.send({
-      data    : { user: { username: user.username, name: user.name } },
-      error   : null,
-      message : 'login successful'
+      data  : { user: { username: user.username, name: user.name } },
+      error : null
     });
   } catch (error) {
     console.error(error);
