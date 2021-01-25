9b8eeea695ff7bb9429b98ee32ce73865d56f426
Archit Bhonsle <abhonsle2000@gmail.com>
2021-01-25 16:39:48 +0530

    🔧  persistent cookie

--- server/index.js
+++ server/index.js
@@ -1,55 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-require('dotenv').config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+require("dotenv").config();
 
 // Route imports
-const authRoutes = require('./routes/auth');
+const authRoutes = require("./routes/auth");
 
 // Constants
-const clientURL = process.env.CLIENT_URL || 'http://localhost:3000',
+const clientURL = process.env.CLIENT_URL || "http://localhost:3000",
   port = process.env.PORT || 4000,
-  dbCollection = process.env.DB_NAME || 'etamax-admin',
+  dbCollection = process.env.DB_NAME || "etamax-admin",
   mongoURL = `mongodb://localhost/${dbCollection}`,
-  sessionSecret = process.env.SESSION_URL || 'etamin';
+  sessionSecret = process.env.SESSION_URL || "etamin";
 
 mongoose.connect(mongoURL, {
-  useNewUrlParser    : true,
-  useUnifiedTopology : true,
-  useCreateIndex     : true,
-  useFindAndModify   : false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.use(
   cors({
-    origin      : clientURL,
-    credentials : true
+    origin: clientURL,
+    credentials: true,
   })
 );
 app.use(
   session({
-    name              : 'etamin',
-    secret            : sessionSecret,
-    resave            : 'false',
-    saveUninitialized : true,
-    cookie            : {
-      httpOnly : true,
-      secure   : false,
-      sameSite : 'lax'
+    name: "etamin",
+    secret: sessionSecret,
+    resave: "false",
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
     },
-    store             : new MongoStore({
-      mongooseConnection : mongoose.connection
-    })
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+    }),
   })
 );
 app.use(express.json());
 
-require('./seed');
+require("./seed");
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`🚀 Up at http://localhost:${port}`);
