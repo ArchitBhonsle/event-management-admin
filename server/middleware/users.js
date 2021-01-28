ab757d065fdf1ac01c6ddda3727390ae0de96ebd
vedpatil611 <vedpatil611@gmail.com>
2021-01-28 19:00:14 +0530

    test api

--- server/middleware/users.js
+++ server/middleware/users.js
@@ -1,6 +1,9 @@
 const User = require("../models/user");
 
-function getUser(rollNo) {
-    const regex = new RegExp(rollno, 'i');
-    var users = User.find({rollno: regex}).sort();
+module.exports = (rollNo, callback) => {
+    const regex = new RegExp(rollNo, 'i');
+    var users = User.find({rollno: Number(rollNo)}, (err, docs) => {
+        callback(err, docs);
+    });
+    return users;
 }
