a63eb00437183a70350a1aae1ded4d100dd91780
vedpatil611 <vedpatil611@gmail.com>
2021-02-03 00:31:48 +0530

    delete user

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { getUserCountFromRollNo } = require('../utils/userQueries');
 const router = express.Router();
 const userQueries = require('../utils/userQueries');
+const { use } = require('./auth');
 
 router.get('/', (req, res) => {
   const search = req.query.search;
   const page = req.query.page;
   const pageLimit = 30;
 
-  let result = { data: {}, error: {} };
+  let result = { data: {}, error: null };
   try {
     (async () => {
       await userQueries.getUserFromRollNo(search, 'rollNo criteria moneyOwed department name', page, pageLimit)
@@ -17,7 +18,8 @@ router.get('/', (req, res) => {
           await docs;
       });
        
-      await userQueries.getUserCountFromRollNo(search).then(async (count) => {
+      await userQueries.getUserCountFromRollNo(search)
+        .then(async (count) => {
         result.data.maxPage = Math.ceil(count / pageLimit);
         await count;
       });
@@ -32,18 +34,19 @@ router.get('/', (req, res) => {
 router.get('/:rollNo', (req, res) => {
   const rollNo = req.params.rollNo;
 
-  userQueries.getUserFromRollNo(
-    rollNo,
-    'rollNo criteria moneyOwed department name email events',
-    (err, docs) => {
-      if (!err)
-        res.send({
-          data: docs[0],
-          error: null,
-        });
-      else throw err;
-    }
-  );
+  userQueries.getUserFromRollNo(rollNo, 'rollNo criteria moneyOwed department name email events')
+    .then((docs) => {
+      res.send({
+        data: docs[0],
+        error: null,
+      });
+    })
+});
+
+router.delete('/:rollNo', (req, res) => {
+  const rollNo = req.params.rollNo;
+
+
 });
 
 module.exports = router;
--- server/utils/userQueries.js
+++ server/utils/userQueries.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const Event = require('../models/event');
 
 module.exports = {
   getUserFromRollNo: (rollNo, fields, page, pageLimit) => {
@@ -23,6 +24,21 @@ module.exports = {
       .exec()
       .then((count) => {
         return count;
-      });
+    });
   },
+  deleteUser: (rollNo) => {
+    (async () => {
+      await User.find({ rollNo: rollNo }, 'events')
+      .exec()
+      .then((docs) => {
+        for(let i=0;i<docs.data.events.length;++i) {
+          docs.data.events[i].seat--;
+        }
+        docs.save();
+      })
+      await User.deleteOne({ rollNo: rollNo }, (err) => {
+        if(err) console.log(err);
+      })
+    })();
+  }
 };
