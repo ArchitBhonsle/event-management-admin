6b022b10c70816cd455e5eac120d30419970a0a2
Archit Bhonsle <abhonsle2000@gmail.com>
2021-02-02 17:41:30 +0530

    🔧  USERS page inside usersList

--- web/src/pages/Users.jsx
+++ web/src/pages/Users.jsx
@@ -7,12 +7,14 @@ import {
   useBreakpointValue,
   IconButton,
   Center,
+  Spinner,
+  Flex,
+  Text,
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { MdSearch } from 'react-icons/md';
 import useSWR from 'swr';
 
-import Layout from '../components/Layout';
 import PageControls from '../components/PageControls';
 import UserCard from '../components/UserCard';
 import createGetUri from '../utils/createGetUri';
@@ -27,27 +29,47 @@ export default function Users() {
   const [searchText, setSearchText] = useState('');
   const [search, setSearch] = useState('');
   const [page, setPage] = useState(1);
-  const [maxPage, setMaxPage] = useState(1);
   const bigSearch = useBreakpointValue({ base: false, md: true });
 
   const { data, error } = useSWR(getFetchUri(search, page));
 
   let usersList = null;
   if (error) {
-    usersList = <Center>Something went wrong</Center>;
+    usersList = (
+      <Center>
+        <Text>Something went wrong</Text>
+      </Center>
+    );
   } else if (!data) {
-    usersList = <Center>Loading...</Center>;
+    usersList = (
+      <Flex w='100%' justifyContent='center'>
+        <Spinner size='lg' colorScheme='green' />
+      </Flex>
+    );
   } else {
-    usersList = data.data.users.map(user => (
-      <UserCard key={user.rollNo} user={user} />
-    ));
+    usersList = (
+      <>
+        <Grid
+          gridTemplateColumns={{
+            base: '1fr',
+            md: 'repeat(2, 1fr)',
+            xl: 'repeat(3, 1fr)',
+          }}
+          gap={4}
+          w='100%'
+        >
+          {data.data.users.map(user => (
+            <UserCard key={user.rollNo} user={user} />
+          ))}
+        </Grid>
+        <PageControls
+          page={page}
+          changePage={newPage => setPage(newPage)}
+          maxPage={data.data.maxPage}
+        />
+      </>
+    );
   }
-
-  useEffect(() => {
-    if (data?.data?.maxPage) {
-      setMaxPage(data?.data?.maxPage);
-    }
-  }, [data]);
 
   function setNewSearch(newSearchText) {
     setSearch(newSearchText);
@@ -59,7 +81,7 @@ export default function Users() {
       <HStack w='100%'>
         <Input
           type='text'
-          placeholder='Roll Number or Email'
+          placeholder='Roll Number'
           size='lg'
           colorScheme='green'
           value={searchText}
@@ -86,22 +108,7 @@ export default function Users() {
           ></IconButton>
         )}
       </HStack>
-      <Grid
-        gridTemplateColumns={{
-          base: '1fr',
-          md: 'repeat(2, 1fr)',
-          xl: 'repeat(3, 1fr)',
-        }}
-        gap={4}
-        w='100%'
-      >
-        {usersList}
-      </Grid>
-      <PageControls
-        page={page}
-        changePage={newPage => setPage(newPage)}
-        maxPage={maxPage}
-      />
+      {usersList}
     </VStack>
   );
 }
