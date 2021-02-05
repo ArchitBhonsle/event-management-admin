6dd160a16604bb5c98d742fd5dabbe63b76ffbe2
Archit Bhonsle <abhonsle2000@gmail.com>
2021-02-05 19:30:54 +0530

    🛠️ schema updates

--- server/models/payment.js
+++ server/models/payment.js
@@ -1,19 +1,27 @@
 const mongoose = require('mongoose');
+const User = require('./user');
 
 const paymentSchema = new mongoose.Schema({
   adminUsername: {
     type: String,
     required: true,
   },
-  rollNo: {
-    type: String,
+  userRollNo: {
+    type: { type: Schema.Types.ObjectId, ref: User },
     required: true,
   },
   amount: {
     type: Number,
     required: true,
   },
+  time: {
+    type: Date,
+    default: Date.now,
+    index: true,
+  },
 });
+
+paymentSchema.index({ rollNo: 1, adminUsername: 1 });
 
 const Payment = mongoose.model('Payment', paymentSchema);
 
