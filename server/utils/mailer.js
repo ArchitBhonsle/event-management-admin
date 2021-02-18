c3e6444a54a1aa3d6b9abf6f703bae09d7192315
Archit Bhonsle <abhonsle2000@gmail.com>
2021-02-18 14:27:05 +0530

    🔧 new link for email

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -37,7 +37,7 @@ module.exports = async (email, rollNo) => {
       `<p>And the password is <strong> ${password} </strong> </p>` +
       `<hr>` +
       `<p>
-      Lorem ipsum dolor sit <a style="color:inherit;font-style:oblique;text-decoration:none;pointer-events:none;" href="bit.do/YeetYeet" target="_blank">hello</a> amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit <a style="color:inherit;font-style:oblique;text-decoration:none;pointer-events:none;" href="http://bit.do/puipuirollpui" target="_blank">hello</a> amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>`,
   });
 
