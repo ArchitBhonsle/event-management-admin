123e353731cbd05bae89d12487422bbf316b5cac
vedpatil611 <vedpatil611@gmail.com>
2021-02-20 04:21:16 +0530

    Mailer changes

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -32,17 +32,16 @@ module.exports = async (email, rollNo) => {
     to: email,
     subject: 'Login and password for Etamax 2021',
     html:
-      `<h1>Etamax 2021</h1>` +
-      `<p>
-      Dearest Agnelites, <br>
-
-      Hope all of you have been safe and having a blast. ✨<a style="color:inherit;text-decoration:none;pointer-events:none;" href="http://bit.do/puipuirollpui" target="_blank">🤩</a>
-      As the dates of etamax are closing by the registrations will be starting.
-      The username and password has been provided below. Start registering for the events of your choice. 🔥😇 
-      </p>` +
-      `<hr>` +
-      `<p>Your identification number is <strong> ${rollNo} </strong> </p>` +
-      `<p>And the password is <strong> ${password} </strong> </p>`,
+    `<h1>Etamax 2021</h1>` +
+    `<p>
+    Dearest Agnelites, <br>
+    Hope all of you have been safe and having a blast. ✨<a style="color:inherit;text-decoration:none;pointer-events:none;" href="http://bit.do/puipuirollpui" target="_blank">🤩</a>
+    As the dates of etamax are closing by the registrations will be starting.
+    The username and password has been provided below. Start registering for the events of your choice. 🔥😇 
+    </p>` +
+    `<hr>` +
+    `<p>Your identification number is <strong> ${rollNo} </strong> </p>` +
+    `<p>And the password is <strong> ${password} </strong> </p>`,
   });
 
   return password;
