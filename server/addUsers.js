70a73f36b39a92095a220c1c3e3fc4c48ad15a73
vedpatil611 <vedpatil611@gmail.com>
2021-02-19 21:43:06 +0530

    add user connection fix

--- server/addUsers.js
+++ server/addUsers.js
@@ -2,11 +2,17 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const csv = require('csv-parser');
 const User = require('./models/user');
-const mailer = require('./utils/mailer');
 const userQueries = require('./utils/userQueries');
+const nodemailer = require('nodemailer');
+const { rword } = require('rword');
+const { randomNumber } = require('./utils/random');
+require('dotenv').config();
 
 const dbCollection = process.env.DB_NAME || 'etamax-admin',
 mongoURL = `mongodb://localhost/${dbCollection}`;
+
+const senderEmail = process.env.MAIL;
+const senderPass = process.env.PASS;
 
 mongoose.connect(mongoURL, {
   useNewUrlParser: true,
@@ -19,14 +25,45 @@ mongoose.connect(mongoURL, {
   try {
     const [, , file] = process.argv;
     
+    const trasporter = nodemailer.createTransport({
+      service: 'hotmail',
+      auth: {
+        user: senderEmail,
+        pass: senderPass,
+      },
+      tls: {
+        ciphers: 'SSLv3',
+      },
+      pool: true,
+      maxConnections: 3,
+      maxMessages: 1,
+    });
+
     await fs
     .createReadStream(file)
     .pipe(csv())
     .on('data', async (data) => {
       try {
-        console.log(await User.findOne({rollNo: data.roll}));
+        const words = rword.generate(2, {
+          length: '3-4',
+          capitalize: 'first',
+        });
+        const password = words[0] + words[1] + randomNumber(100, 999).toString();
         const user = await userQueries.generateUserR(data.roll, data.email);
-        const password = await mailer(data.email, data.roll);
+        
+        await trasporter.sendMail({
+          from: senderEmail,
+          to: data.email,
+          subject: 'Login and password for Etamax 2021',
+          html:
+            `<h1>Etamax 2021</h1>` +
+            `<p>Your identification number is <strong> ${data.roll} </strong> </p>` +
+            `<p>And the password is <strong> ${password} </strong> </p>` +
+            `<hr>` +
+            `<p>
+            Lorem ipsum dolor sit <a style="color:inherit;font-style:oblique;text-decoration:none;pointer-events:none;" href="http://bit.do/puipuirollpui" target="_blank">hello</a> amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>`,
+        });
         User.register(user, password);
       } catch (err) {
         console.log(err);
